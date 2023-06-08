52f362320894cc75522652017ce9b93f68f1e077
Olesya Strinada <128924160+OStrinada@users.noreply.github.com>
2023-06-08 17:59:29 +0700

    Update practic.py

--- practic.py
+++ practic.py
@@ -1,32 +1,91 @@
-overall_cost = 0
+error = 'ПЕРЕЗАПУСТИТЕ ПРОГРАММУ И ВВЕДИТЕ КОРРЕКТНЫЕ ДАННЫЕ.'
+
+sequence_num = input('Введите последовательность целых чисел через пробел: ')
+
 while True:
     try:
-        tickets = int(input('Сколько билетов хотите приобрести?'))
-        if type(tickets) == int:
+        some_num = int(input('Введите любое целое число: '))
+        if type(some_num) == int:
             break
     except ValueError:
-        print('Введите целое число...')
-for i in range(tickets):
-    i += 1
-    while True:
-        try:
-            age_visitor = input(f'Укажите возраст обладателя билета №{i}? ')
-            age_visitor = int(age_visitor)
-            if age_visitor < 18:
-                print('Билет бесплатный.')
-            elif 25 > age_visitor >= 18:
-                overall_cost += 990
-                print('Стоимость билета: 990 руб.')
-            else:
-                overall_cost += 1390
-                print('Стоимость билета: 1390 руб.')
-            if type(age_visitor) == int:
-                break
-        except ValueError:
-            print('Введите целое число...')
-if tickets > 3:
-    overall_cost = overall_cost - ((overall_cost / 100) * 10)
-    print(f'Сумма к оплате {overall_cost} руб. с учетом 10%-ой скидки за регистрацию более 3-х человек.')
+        print('\nВведите число в соответствии с условиями ввода.')
+def is_int(string):
+    string = string.replace(' ', '')
+    try:
+        int(string)
+        return True
+    except ValueError:
+        return False
+
+if " " not in sequence_num:
+    print("\nВ вводе отсутствуют пробелы. Введите числа в соответствии с условиям ввода.")
+    sequence_num = input('Введите последовательность целых чисел через пробел: ')
+if not is_int(sequence_num):
+    print('\nВ вводе последовательности чисел содержатся не цифры либо введены не целые числа.\nВведите числа в соответствии с условиям ввода.')
+    print(error)
 else:
-    print(f'Сумма к оплате {overall_cost} руб.')
-print('Не забудьте документы, подтверждающие возраст участника!')
+    sequence_num = sequence_num.split()
+
+list_sequence_num = [int(item) for item in sequence_num]
+
+def merge_sort(L):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = len(L) // 2
+        left = merge_sort(L[:middle])
+        right = merge_sort(L[middle:])
+        return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+list_sequence_num = merge_sort(list_sequence_num)
+
+def binary_search(array, element, left, right):
+    try:
+        if left > right:
+            return False
+        middle = (right + left) // 2
+        if array[middle] == element:
+            return middle
+        elif element < array[middle]:
+            return binary_search(array, element, left, middle - 1)
+        else:
+            return binary_search(array, element, middle + 1, right)
+    except IndexError:
+        return 'Число выходит за диапазон списка.\nПерезапустите программу и введите меньшее число.'
+
+print(f'Упорядоченный список: {list_sequence_num}')
+
+if not binary_search(list_sequence_num, some_num, 0, len(list_sequence_num)):
+    rI = min(list_sequence_num, key=lambda x: (abs(x - some_num), x))
+    ind = list_sequence_num.index(rI)
+    max_ind = ind + 1
+    min_ind = ind - 1
+    if rI < some_num:
+        print(f'Ближайший меньший элемент: {rI}, его индекс: {ind}')
+    elif min_ind < 0:
+        print(f'''В списке нет меньшего элемента.
+Ближайший больший элемент: {rI}, его индекс: {list_sequence_num.index(rI)}
+Перезапустите программу и введите другое число.''')
+else:
+    print(f'Индекс введенного элемента: {binary_search(list_sequence_num, some_num, 0, len(list_sequence_num))}')
